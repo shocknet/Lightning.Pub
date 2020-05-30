6391db0194fb8845d4083e80393a9145d86db1e2
Daniel Lugo <danlugo92@gmail.com>
2020-05-30 16:26:57 -0400

    move import to top

--- src/routes.js
+++ src/routes.js
@@ -33,6 +33,8 @@ module.exports = async (
   mySocketsEvents,
   { serverPort, CA, CA_KEY, usetls }
 ) => {
+  const {timeout5} = require('../services/gunDB/contact-api/utils')
+
   const Http = Axios.create({
     httpsAgent: new httpsAgent.Agent({  
       ca: await FS.readFile(CA)
@@ -1675,7 +1677,6 @@ module.exports = async (
       res.json(channelBackups);
     });
   });
-  const {timeout5} = require('../services/gunDB/contact-api/utils')
   const GunEvent = Common.Constants.Event
   const Key = require('../services/gunDB/contact-api/key')
   
@@ -1707,7 +1708,7 @@ module.exports = async (
   })
 
   const Events = require('../services/gunDB/contact-api/events')
-  
+
   app.get(`/api/gun/${GunEvent.ON_RECEIVED_REQUESTS}`, (_, res) => {
     try {
       // spinup
