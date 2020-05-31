6f87d214ecc9ea5a8374105163e0fb00b6b67717
Daniel Lugo <danlugo92@gmail.com>
2020-05-31 12:17:22 -0400

    correct  wording

--- src/routes.js
+++ src/routes.js
@@ -1949,7 +1949,7 @@ module.exports = async (
   }
 
   ap.get('/api/gun/follows', apiGunFollowsGet)
-  ap.post(`/api/gun/follow`, apiGunFollowsPost)
+  ap.post(`/api/gun/follows`, apiGunFollowsPost)
   ap.delete(`/api/gun/follows/:publickey`, apiGunFollowsDelete)
 
   /**
