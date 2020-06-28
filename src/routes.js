ba34ac20bc5799299e15191371b037beea50ee8f
Daniel Lugo <danlugo92@gmail.com>
2020-06-27 23:00:26 -0400

    typo

--- src/routes.js
+++ src/routes.js
@@ -1975,7 +1975,7 @@ module.exports = async (
     */
    const apiGunFeedGet = async (req, res) => {
     try {
-      const { pageStr } = req.query;
+      const { page: pageStr } = req.query;
       const page = Number(pageStr)
 
       if (!isARealUsableNumber(page)) {
