8ee5c287e4fbad52d9f4a558e7c90bf222ae787e
Erfan Besharat <erbesharat@gmail.com>
2020-07-04 02:18:51 +0430

    add PoC test handler

---
+++ tests/gun/gun.js
@@ -0,0 +1,77 @@
+// @ts-check
+const Gun = require("gun");
+
+const ALIAS_AND_PASS = Math.round(Math.random() * 10000).toString();
+
+/**
+ * Runs mimiza's fork.
+ */
+const FORK_SUPER_PEER = "http://gun.shock.network:8765/gun";
+
+/**
+ * Runs mimiza's fork.
+ */
+const AUX_SUPER_PEER = "";
+
+const Peer = {
+  NO_PEERS: [],
+  OUR_USUAL_PEER: [FORK_SUPER_PEER],
+  SHOCK_PLUS_ANOTHER: [FORK_SUPER_PEER, AUX_SUPER_PEER],
+};
+
+// eslint-disable-next-line init-declarations
+let gun, user;
+
+const freshGun = async () => {
+  console.log(`\n creating fresh gun`);
+  user = null;
+
+  gun = new Gun({
+    axe: false,
+    peers: Peer.OUR_USUAL_PEER,
+  });
+
+  user = gun.user();
+
+  const pub = await new Promise((res, rej) => {
+    user.create(ALIAS_AND_PASS, ALIAS_AND_PASS, (ack) => {
+      if (ack.err === `User already created!`) {
+        res(
+          new Promise((res, rej) => {
+            user.auth(ALIAS_AND_PASS, ALIAS_AND_PASS, (ack) => {
+              if (ack.err) {
+                rej(ack.err);
+              } else {
+                res(ack.sea.pub);
+              }
+            });
+          })
+        );
+
+        return;
+      }
+
+      if (ack.err) {
+        rej(new Error(ack.err));
+      } else {
+        res(ack.pub);
+      }
+    });
+  });
+
+  return {
+    gun,
+    user,
+    pub,
+  };
+};
+
+const getGun = () => gun;
+
+const getUser = () => user;
+
+module.exports = {
+  freshGun,
+  getGun,
+  getUser,
+};
---
+++ tests/utils/utils.js
@@ -0,0 +1,148 @@
+// @ts-check
+const Gun = require("../gun/gun");
+const colors = require('colors');
+const TIMEOUT_ERR = "Timeout Error";
+
+/**
+ * @param {number} ms
+ * @returns {Promise<void>}
+ */
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+/**
+ * @template T
+ * @param {Promise<T>} promise
+ * @param {Number} timeout
+ * @returns {Promise<T>}
+ */
+const timeout = (promise, timeout) => {
+  /** @type {NodeJS.Timeout} */
+  // eslint-disable-next-line init-declarations
+  let timeoutID;
+
+  return Promise.race([
+    promise.then((v) => {
+      clearTimeout(timeoutID);
+      return v;
+    }),
+
+    new Promise((_, rej) => {
+      timeoutID = setTimeout(() => {
+        rej(new Error(TIMEOUT_ERR));
+      }, timeout);
+    }),
+  ]);
+};
+
+/**
+ * @template T
+ * @param {(gun: any, user: any) => Promise<T>} promGen The function
+ * receives the most recent gun and user instances.
+ * @param {((resolvedValue: unknown) => boolean)=} shouldRetry
+ * @returns {Promise<T>}
+ */
+const tryAndWait = async (promGen, shouldRetry = () => false) => {
+  /* eslint-disable no-empty */
+  /* eslint-disable init-declarations */
+
+  // If hang stop at 10, wait 3, retry, if hang stop at 5, reinstate, warm for
+  // 5, retry, stop at 10, err
+
+  /** @type {T} */
+  let resolvedValue;
+
+  try {
+    resolvedValue = await timeout(promGen(Gun.getGun(), Gun.getUser()), 10000);
+
+    if (shouldRetry(resolvedValue)) {
+      console.log(
+        "force retrying" +
+          ` args: ${promGen.toString()} -- ${shouldRetry.toString()}`
+      );
+    } else {
+      return resolvedValue;
+    }
+  } catch (e) {
+    console.log(e);
+    if (e.message === "NOT_AUTH") {
+      throw e;
+    }
+  }
+
+  console.log(
+    `\n retrying \n` +
+      ` args: ${promGen.toString()} -- ${shouldRetry.toString()}`
+  );
+
+  await delay(3000);
+
+  try {
+    resolvedValue = await timeout(promGen(Gun.getGun(), Gun.getUser()), 5000);
+
+    if (shouldRetry(resolvedValue)) {
+      console.log(
+        "force retrying" +
+          ` args: ${promGen.toString()} -- ${shouldRetry.toString()}`
+      );
+    } else {
+      return resolvedValue;
+    }
+  } catch (e) {
+    console.log(e);
+    if (e.message === "NOT_AUTH") {
+      throw e;
+    }
+  }
+
+  console.log(
+    `\n recreating a fresh gun and retrying one last time \n` +
+      ` args: ${promGen.toString()} -- ${shouldRetry.toString()}`
+  );
+
+  const { gun, user } = await Gun.freshGun();
+
+  return timeout(promGen(gun, user), 10000);
+};
+
+/**
+ * @param {any} log
+ * @param {import('../index').Handler} handler
+ */
+const LogWriter = (log, handler) => {
+  switch (log.level) {
+    case 'notice':
+      console.log(
+        colors.bgGreen('SUCCESS:'),
+        colors.white.underline(log.message)
+      )
+      handler.State.Pass += 1
+      break;
+  
+    case 'error':
+      console.log(
+        colors.bgRed.white('FAILURE:'),
+        colors.white.underline(log.message)
+      )
+      handler.State.Fail += 1
+      break;
+
+     case 'info':
+      console.log(
+        colors.bgWhite.black('INFO:'),
+        colors.white(log.message)
+      )
+      break;
+    default:
+      console.log(
+        colors.grey(log.message)
+      );
+      break;
+  }
+  
+  // TODO_ADD: An map tree which includes failure, success and asserts
+}
+
+module.exports = {
+  tryAndWait,
+  LogWriter
+};
