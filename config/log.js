7228a703eb249b4a4064f70a65e9cfef02d041c0
emad-salah <emads14@gmail.com>
2020-07-28 19:20:30 +0100

    Formatted logs with colors!

--- config/log.js
+++ config/log.js
@@ -14,13 +14,27 @@ module.exports = (logFileName, logLevel) => {
   if (!winstonAttached.has(logFileName)) {
     winston.add(new (winston.transports.DailyRotateFile)({
       filename: logFileName,
-      datePattern: "yyyy-MM-dd.",
+      datePattern: "yyyy-MM-DD",
       json: false,
       maxSize: 1000000,
       maxFiles: 7,
       level: logLevel
     }))
-    winston.add(new winston.transports.Console())
+    winston.add(new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.align(),
+        winston.format.printf((info) => {
+          const {
+            timestamp, level, message, ...args
+          } = info;
+
+          const ts = timestamp.slice(0, 19).replace('T', ' ');
+          return `${ts} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
+        }),
+      )
+    }))
     winston.level = logLevel
     winstonAttached.set(logFileName, winston)
   }
