d1734a01c01f3207b8f751a1b31dc3f3a5a3e37d
Daniel Lugo <danlugo92@gmail.com>
2020-08-26 21:09:47 -0400

    avoid dup order processing

--- services/gunDB/contact-api/jobs/onOrders.js
+++ services/gunDB/contact-api/jobs/onOrders.js
@@ -19,6 +19,11 @@ const Utils = require('../utils')
 const getUser = () => require('../../Mediator').getUser()
 
 /**
+ * @type {Set<string>}
+ */
+const ordersProcessed = new Set()
+
+/**
  * @typedef {import('../SimpleGUN').GUNNode} GUNNode
  * @typedef {import('../SimpleGUN').ListenerData} ListenerData
  * @typedef {import('../SimpleGUN').ISEA} ISEA
@@ -45,6 +50,15 @@ const listenerForAddr = (addr, SEA) => async (order, orderID) => {
       logger.info(`Expected an order instead got: ${JSON.stringify(order)}`)
       return
     }
+
+    if (ordersProcessed.has(orderID)) {
+      logger.warn(
+        `skipping already processed order: ${orderID}, this means orders are being processed twice!`
+      )
+      return
+    }
+
+    ordersProcessed.add(orderID)
 
     logger.info(
       `onOrders() -> processing order: ${orderID} -- ${JSON.stringify(
