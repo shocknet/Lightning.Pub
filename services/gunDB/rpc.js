594ebc8078cecffb0700227434298b3d42213434
Daniel Lugo <danlugo92@gmail.com>
2020-10-06 12:42:58 -0400

    gun rpc for writes

---
+++ services/gunDB/rpc.js
@@ -0,0 +1,106 @@
+/**
+ * @format
+ */
+// @ts-check
+const { makePromise, Constants } = require('shock-common')
+
+const { getGun, getUser } = require('./Mediator')
+
+/**
+ * @param {string} rawPath
+ * @param {import('./contact-api/SimpleGUN').ValidDataValue} value
+ * @returns {Promise<void>}
+ */
+const put = async (rawPath, value) => {
+  const [root, ...path] = rawPath.split('.')
+
+  const node = (() => {
+    // eslint-disable-next-line init-declarations
+    let _node
+
+    if (root === '$gun') {
+      _node = getGun()
+    } else if (root === '$user') {
+      const u = getUser()
+
+      if (!u.is) {
+        throw new Error(Constants.ErrorCode.NOT_AUTH)
+      }
+
+      _node = u
+    } else {
+      throw new TypeError(
+        `Unknown kind of root, expected $gun or $user but got: ${root}`
+      )
+    }
+
+    for (const bit of path) {
+      _node = _node.get(bit)
+    }
+
+    return _node
+  })()
+
+  await makePromise((res, rej) => {
+    node.put(value, ack => {
+      if (ack.err && typeof ack.err !== 'number') {
+        rej(new Error(ack.err))
+      } else {
+        res()
+      }
+    })
+  })
+}
+
+/**
+ * @param {string} rawPath
+ * @param {import('./contact-api/SimpleGUN').ValidDataValue} value
+ * @returns {Promise<string>}
+ */
+const set = async (rawPath, value) => {
+  const [root, ...path] = rawPath.split('.')
+
+  const node = (() => {
+    // eslint-disable-next-line init-declarations
+    let _node
+
+    if (root === '$gun') {
+      _node = getGun()
+    } else if (root === '$user') {
+      const u = getUser()
+
+      if (!u.is) {
+        throw new Error(Constants.ErrorCode.NOT_AUTH)
+      }
+
+      _node = u
+    } else {
+      throw new TypeError(
+        `Unknown kind of root, expected $gun or $user but got: ${root}`
+      )
+    }
+
+    for (const bit of path) {
+      _node = _node.get(bit)
+    }
+
+    return _node
+  })()
+
+  const id = await makePromise((res, rej) => {
+    const subNode = node.set(value, ack => {
+      if (ack.err && typeof ack.err !== 'number') {
+        rej(new Error(ack.err))
+      } else {
+        res(subNode._.get)
+      }
+    })
+  })
+
+  return id
+}
+
+module.exports = {
+  put,
+  set
+}
