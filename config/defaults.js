3c6aceb1ad77819e874f41ad5a76b5cb6f823db2
hatim boufnichel <boufni95@gmail.com>
2020-10-17 18:41:03 +0200

    fetch user data endpoint

--- config/defaults.js
+++ config/defaults.js
@@ -46,7 +46,7 @@ module.exports = (mainnet = false) => {
     logfile: "shockapi.log",
     lndLogFile: parsePath(`${lndDirectory}/logs/bitcoin/${network}/lnd.log`),
     lndDirPath: lndDirectory,
-    peers: ['http://gun.shock.network:8765/gun','http://gun2.shock.network:8765/gun'],
+    peers: ["http://superpeer:8765/gun"],
     useTLS: false,
     tokenExpirationMS: 259200000
   }; 
--- src/routes.js
+++ src/routes.js
@@ -2285,6 +2285,28 @@ module.exports = async (
       })
     }
   })
+
+  app.post(`/api/gun/userInfo`, async (req, res) => {
+    try {
+      const { pubs } = req.body
+      const reqs = pubs.map(
+        e =>
+          new Promise((res, rej) => {
+            GunGetters.getUserInfo(e)
+              .then(r => res(r))
+              .catch(e => rej(e))
+          })
+      )
+      const infos = await Promise.all(reqs)
+      return res.status(200).json({
+        pubInfos: infos
+      })
+    } catch (err) {
+      return res.status(500).json({
+        errorMessage: err.message
+      })
+    }
+  })
   /////////////////////////////////
   /**
    * @template P
