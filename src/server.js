6e9d90ed60ed430b2a5c381f3e4111399e935b08
Emad Kheir <emads14@gmail.com>
2020-11-17 17:46:44 +0100

    Add custom config paths

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ const server = program => {
 
   // load app default configuration data
   const defaults = require('../config/defaults')(program.mainnet)
-  const rootFolder = process.resourcesPath || __dirname
+  const rootFolder = program.rootPath || process.resourcesPath || __dirname
 
   // define env variables
   Dotenv.config()
@@ -216,8 +216,10 @@ const server = program => {
         next()
       })
 
+      const storageDirectory = Path.resolve(rootFolder, `${program.rootPath ? '.' : '..'}/.storage`);
+
       await Storage.init({
-        dir: Path.resolve(rootFolder, '../.storage')
+        dir: storageDirectory
       })
 
       const getSessionSecret = async () => {
