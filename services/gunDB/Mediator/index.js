6e0ffa0d6f8212b648f4172701aceedec0ab3a52
Daniel Lugo <danlugo92@gmail.com>
2021-09-12 15:02:12 -0400

    No new

--- services/gunDB/Mediator/index.js
+++ services/gunDB/Mediator/index.js
@@ -479,7 +479,7 @@ const instantiateGun = () => {
   // @ts-ignore
   gun = null
 
-  const _gun = /** @type {unknown} */ (new Gun({
+  const _gun = /** @type {unknown} */ (Gun({
     axe: false,
     multicast: false,
     peers: Config.PEERS
