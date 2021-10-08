2f16f0db4ba9d7d8f423f22a497580c1ed828d79
Daniel Lugo <danlugo92@gmail.com>
2021-10-08 14:56:42 -0400

    Use specialThen() here

--- services/gunDB/contact-api/getters/index.js
+++ services/gunDB/contact-api/getters/index.js
@@ -3,19 +3,17 @@
  */
 
 const Key = require('../key')
-const Utils = require('../utils')
 
 /**
  * @param {string} pub
  * @returns {Promise<string>}
  */
 exports.currentOrderAddress = async pub => {
-  const currAddr = await Utils.tryAndWait(gun =>
-    gun
-      .user(pub)
-      .get(Key.CURRENT_ORDER_ADDRESS)
-      .then()
-  )
+  const currAddr = await require('../../Mediator')
+    .getGun()
+    .user(pub)
+    .get(Key.CURRENT_ORDER_ADDRESS)
+    .specialThen()
 
   if (typeof currAddr !== 'string') {
     throw new TypeError('Expected user.currentOrderAddress to be an string')
