e1bd05f7a57586fdd08895667a2636198f78af77
Daniel Lugo <danlugo92@gmail.com>
2021-10-09 09:34:34 -0400

    Stuff for https

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ process.on('uncaughtException', e => {
  */
 const server = program => {
   const Http = require('http')
+  const Https = require('https')
+  const FS = require('fs')
   const Express = require('express')
   const Crypto = require('crypto')
   const Dotenv = require('dotenv')
@@ -294,20 +296,20 @@ const server = program => {
         res.status(500).send({ status: 500, errorMessage: 'internal error' })
       })
 
-      const CA = LightningServices.servicesConfig.lndCertPath
-      const CA_KEY = CA.replace('cert', 'key')
+      const CA =
+        program.httpsCert || LightningServices.servicesConfig.lndCertPath
+      const CA_KEY = program.httpsCertKey || CA.replace('cert', 'key')
 
       const createServer = () => {
         try {
-          // if (LightningServices.servicesConfig.lndCertPath && program.usetls) {
-          //   const [key, cert] = await Promise.all([
-          //     FS.readFile(CA_KEY),
-          //     FS.readFile(CA)
-          //   ])
-          //   const httpsServer = Https.createServer({ key, cert }, app)
+          if (LightningServices.servicesConfig.lndCertPath && program.useTLS) {
+            const key = FS.readFileSync(CA_KEY, 'utf-8')
+            const cert = FS.readFileSync(CA, 'utf-8')
 
-          //   return httpsServer
-          // }
+            const httpsServer = Https.createServer({ key, cert }, app)
+
+            return httpsServer
+          }
 
           const httpServer = Http.Server(app)
           return httpServer
@@ -357,7 +359,7 @@ const server = program => {
         {
           serverHost,
           serverPort,
-          usetls: program.usetls,
+          useTLS: program.useTLS,
           CA,
           CA_KEY
         }
