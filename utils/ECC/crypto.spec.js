c53bd5f49794176f0b75c28c26e9ebdc9c82bb0d
Daniel Lugo <danlugo92@gmail.com>
2021-12-16 11:07:03 -0400

    Use top level describe()

--- utils/ECC/crypto.spec.js
+++ utils/ECC/crypto.spec.js
@@ -10,25 +10,27 @@ const {
   convertBufferToBase64
 } = require('./crypto')
 
-describe('generateRandomString()', () => {
-  it('creates a random string of the specified length', async () => {
-    expect.hasAssertions()
-    const base = Math.ceil(Math.random() * 100)
-    const len = base % 2 !== 0 ? base + 1 : base
-    const result = await generateRandomString(len)
+describe('crypto', () => {
+  describe('generateRandomString()', () => {
+    it('creates a random string of the specified length', async () => {
+      expect.hasAssertions()
+      const base = Math.ceil(Math.random() * 100)
+      const len = base % 2 !== 0 ? base + 1 : base
+      const result = await generateRandomString(len)
 
-    expect(result.length).toEqual(len)
+      expect(result.length).toEqual(len)
+    })
   })
-})
 
-describe('Buffer <> String <> Buffer', () => {
-  it('preserves values', async () => {
-    const rnd = await generateRandomString(24)
+  describe('Buffer <> String <> Buffer', () => {
+    it('preserves values', async () => {
+      const rnd = await generateRandomString(24)
 
-    const asBuffer = convertBase64ToBuffer(rnd)
+      const asBuffer = convertBase64ToBuffer(rnd)
 
-    const asStringAgain = convertBufferToBase64(asBuffer)
+      const asStringAgain = convertBufferToBase64(asBuffer)
 
-    expect(asStringAgain).toEqual(rnd)
+      expect(asStringAgain).toEqual(rnd)
+    })
   })
 })
