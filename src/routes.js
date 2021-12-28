1af855d019ab8901db0c06338550cad68d46742c
Daniel Lugo <danlugo92@gmail.com>
2021-12-28 12:12:25 -0400

    Typing

--- src/routes.js
+++ src/routes.js
@@ -1296,7 +1296,10 @@ module.exports = async (
       // const limit = page * itemsPerPage;
       lightning.listInvoices(
         { reversed, index_offset: offset, num_max_invoices: itemsPerPage },
-        async (err, { invoices = [], last_index_offset = 1 } = {}) => {
+        async (
+          /** @type {Error} */ err,
+          { invoices = [], last_index_offset = 1 } = {}
+        ) => {
           if (err) {
             logger.debug('ListInvoices Error:', err)
             const health = await checkHealth()
