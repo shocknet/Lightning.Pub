99896dfb7ebd3099280c7613a581a0e2aa10fdf1
Daniel Lugo <danlugo92@gmail.com>
2022-01-17 13:17:12 -0400

    Correct typing for list channels request

--- utils/lightningServices/types.ts
+++ utils/lightningServices/types.ts
@@ -134,15 +134,15 @@ export interface Services {
 }
 
 export interface ListChannelsReq {
-  active_only: boolean
-  inactive_only: boolean
-  public_only: boolean
-  private_only: boolean
+  active_only?: boolean
+  inactive_only?: boolean
+  public_only?: boolean
+  private_only?: boolean
   /**
    * Filters the response for channels with a target peer's pubkey. If peer is
    * empty, all channels will be returned.
    */
-  peer: Common.Bytes
+  peer?: Common.Bytes
 }
 
 /**
