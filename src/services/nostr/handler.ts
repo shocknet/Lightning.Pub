cba2961685621d77dc9ff758bd3eb0854feb1d82
boufni95 <boufni95@gmail.com>
2023-12-14 19:48:46 +0100

    deb

--- src/services/nostr/handler.ts
+++ src/services/nostr/handler.ts
@@ -98,7 +98,7 @@ export default class Handler {
         const log = getLogger({})
         log("conneting to relay...")
         const s = relayInit(this.settings.relays[0]) // TODO: create multiple conns for multiple relays
-        await new Promise<void>(res => s.on('connect', res))
+        await new Promise<void>(res => s.on('connect', () => { console.log("okk"); res() }))
         log("relay connected, subbing...")
         s.on('disconnect', () => {
             log("relay disconnected, will try to reconnect")
