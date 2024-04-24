e5ad3af98676bc724015fd96ef39bfdcac7a8d5a
hatim boufnichel <boufni95@gmail.com>
2024-04-24 19:54:38 +0200

    up

---
+++ src/tests/bitcoinCore.ts
@@ -0,0 +1,37 @@
+// @ts-ignore
+import BitcoinCore from 'bitcoin-core';
+import { TestSettings } from '../services/main/settings';
+export class BitcoinCoreWrapper {
+    core: BitcoinCore
+    addr: { address: string }
+    constructor(settings: TestSettings) {
+        this.core = new BitcoinCore({
+            //network: 'regtest',
+            host: '127.0.0.1',
+            port: `${settings.bitcoinCoreSettings.port}`,
+            username: settings.bitcoinCoreSettings.user,
+            password: settings.bitcoinCoreSettings.pass,
+            // use a long timeout due to the time it takes to mine a lot of blocks
+            timeout: 5 * 60 * 1000,
+        })
+    }
+
+    Init = async () => {
+        const wallet = await this.core.createWallet('');
+        console.log({ wallet })
+        this.addr = await this.core.getNewAddress()
+        console.log({ addr: this.addr })
+        await this.Mine(101)
+        const info = await this.core.getWalletInfo();
+        console.log({ info })
+    }
+
+    Mine = async (blocks: number) => {
+        await this.core.generateToAddress(blocks, this.addr)
+    }
+
+    SendToAddress = async (address: string, amount: number) => {
+        const tx = await this.core.sendToAddress(address, amount)
+        console.log({ tx })
+    }
+}
