9e76e36acb42d3d62f0c2a2308e76292637ddb26
hatim boufnichel <boufni95@gmail.com>
2024-04-24 20:29:18 +0200

    up

--- src/tests/networkSetup.ts
+++ src/tests/networkSetup.ts
@@ -43,7 +43,7 @@ const openChannels = async (core: BitcoinCoreWrapper, instances: LndInstances, i
     await instances.bob.OpenChannel(info.carol.pubkey, addresses.bob, 5_000_000, 0)
     await instances.carol.OpenChannel(info.alice.pubkey, addresses.carol, 5_000_000, 0)
     await instances.alice.OpenChannel(info.dave.pubkey, addresses.alice, 5_000_000, 0)
-    await slowMine(core, 6)
+    await slowMine(core, 10)
 
 }
 
