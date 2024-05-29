bbcff16dd6a72676e76a77d4ab809f9301709607
polarDefender <patch.demon115@gmail.com>
2024-05-29 08:20:04 -0700

    updated ui

--- static/js/backup.js
+++ static/js/backup.js
@@ -1,11 +1,21 @@
 $(() => {
+  let backup; let manual_backup;
   $("#backup").click(() => {
-    const checked = $("#backup").prop("checked");
+    backup = $("#backup").prop("checked");
     const nextButton = $("#next-button");
-    if (checked) {
+    if (backup || manual_backup) {
       nextButton.removeClass("hidden-button");
     } else {
       nextButton.addClass("hidden-button");
     }
+  });
+  $("#manual-backup").click(()=>{
+      manual_backup = $('#manual-backup').prop("checked");
+      const nextButton = $("#next-button");
+      if(backup || manual_backup) {
+        nextButton.removeClass("hidden-button");
+      } else {
+        nextButton.addClass("hidden-button");
+      };
   });
 });
