1ec83e05874c8d92bc27c0241a617b7ffa1c3be0
Justin (shocknet) <justin@shock.network>
2024-07-05 17:36:45 -0400

    deploy

--- scripts/install_nodejs.sh
+++ scripts/install_nodejs.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 install_nodejs() {
+  if [ "$EUID" -eq 0 ] && [ -n "$SUDO_USER" ]; then
+    USER_HOME=$(getent passwd ${SUDO_USER} | cut -d: -f6)
+    USER_NAME=${SUDO_USER}
+  else
+    USER_HOME=$HOME
+    USER_NAME=$(whoami)
+  fi
+
+  NVM_DIR="$USER_HOME/.nvm"
   log "${PRIMARY_COLOR}Checking${RESET_COLOR} for Node.js..."
   MINIMUM_VERSION="18.0.0"
   
@@ -10,7 +19,7 @@ install_nodejs() {
 
   if ! command -v nvm &> /dev/null; then
     NVM_VERSION=$(wget -qO- https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
-    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash > /dev/null 2>&1
+    sudo -u $USER_NAME bash -c "wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash > /dev/null 2>&1"
     export NVM_DIR="${NVM_DIR}"
     [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"
   fi
@@ -27,7 +36,7 @@ install_nodejs() {
     log "Node.js is not installed. ${PRIMARY_COLOR}Installing the LTS version...${RESET_COLOR}"
   fi
 
-  nvm install --lts || {
+  sudo -u $USER_NAME bash -c "source ${NVM_DIR}/nvm.sh && nvm install --lts" || {
     log "${PRIMARY_COLOR}Failed to install Node.js.${RESET_COLOR}"
     exit 1
   }
