c287ac2cb85bb6588f2e414122da03eb73014cc6
boufni95 <boufni95@gmail.com>
2025-03-10 20:34:41 +0000

    up

--- src/services/storage/storageProcessor.ts
+++ src/services/storage/storageProcessor.ts
@@ -333,6 +333,7 @@ class StorageProcessor {
 
     private async handleFindOne(operation: FindOneOperation<any>) {
         const manager = this.getManager(operation.txId);
+        console.log(operation.q.where)
         const res = await manager.getRepository(MainDbEntities[operation.entity]).findOne(operation.q)
 
         this.sendResponse({
