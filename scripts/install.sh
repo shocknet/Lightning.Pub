8b98ce360bab4c3db31a91b411ecad8ebfd9bde3
shocknet-justin <justin@shock.network>
2025-08-26 16:47:17 -0400

    test url

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,7 +16,7 @@ log() {
 
 SCRIPT_VERSION="0.2.0"
 REPO_URL="https://github.com/shocknet/Lightning.Pub/tarball/script"
-BASE_URL="https://raw.githubusercontent.com/shocknet/Lightning.Pub/master/scripts/"
+BASE_URL="https://raw.githubusercontent.com/shocknet/Lightning.Pub/script/scripts/"
 
 cleanup() {
     echo "Cleaning up temporary files..."
